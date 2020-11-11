379b430e23318467424971c589e7fe63a3f83553
Carmenprz <carmenp.gomez99@gmail.com>
2020-11-11 17:03:17 +0100

    copy repository

---
+++ index.js
@@ -0,0 +1,114 @@
+"use strict";
+
+const startButton = document.querySelector("#startButton");
+const resetButton = document.querySelector("#resetButton");
+const addButton = document.querySelector("#addButton");
+
+startButton.addEventListener("click", startWheel);
+resetButton.addEventListener("click", reset);
+addButton.addEventListener("click", addCoder);
+
+let listaCoders = [];
+
+function startWheel() {
+  const listCodersAlive = [];
+  listaCoders.forEach((coder) => {
+    if (coder.status === "alive") {
+      listCodersAlive.push(coder);
+    }
+  });
+  if (listCodersAlive.length <= 0) {
+    showResultMessage("No one left! Please reset");
+    return;
+  }
+  const selectedCoder = selectCoder(listCodersAlive);
+  showResultMessage(selectedCoder.name);
+  updateList(selectedCoder, "dead");
+  updateCoderCard(selectedCoder);
+}
+
+function selectCoder(array) {
+  const selectedCoder = array[Math.floor(Math.random() * array.length)];
+  return selectedCoder;
+}
+
+function updateCoderCard(coder) {
+  const coderCard = document.querySelector(`#${coder.name}`);
+  coderCard.className = `${coder.status}`;
+}
+
+function updateList(coder, status) {
+  const coderIndex = listaCoders.findIndex(
+    (element) => element.name === coder.name
+  );
+  listaCoders[coderIndex].status = status;
+}
+
+function showResultMessage(message) {
+  const selectedCard = document.querySelector("#display-message");
+  selectedCard.innerHTML = message;
+}
+
+function showCoders() {
+  listaCoders.forEach((coder) => {
+    createCoderArticle(coder);
+  });
+}
+
+function createCoderArticle(coder) {
+  const coderListSection = document.querySelector("#coderList");
+  const coderArticle = document.createElement("article");
+  const coderName = document.createElement("p", `${coder.name}`);
+  coderName.innerHTML += `${coder.name}`;
+  coderArticle.setAttribute("class", `${coder.status}`);
+  coderArticle.setAttribute("id", `${coder.name}`);
+  coderArticle.appendChild(coderName);
+  coderListSection.appendChild(coderArticle);
+}
+
+function reset() {
+  listaCoders.forEach((element) => {
+    updateList(element, "alive");
+    updateCoderCard(element);
+  });
+  showResultMessage("");
+}
+
+function addCoder() {
+  let coderNameInput = document.querySelector("#addCoder");
+  const coderName = coderNameInput.value;
+  if (!validateInput(coderName)) {
+    return;
+  }
+  const coder = {
+    name: coderName,
+    status: "alive",
+  };
+  listaCoders.push(coder);
+  createCoderArticle(coder);
+  coderNameInput.value = "";
+}
+
+function validateInput(value) {
+  if (value === "" || value === undefined) {
+    showFeedbackMessage();
+    return;
+  }
+  return true;
+}
+
+function showFeedbackMessage() {
+  window.alert("please insert name")
+}
+
+const audio = document.getElementById("myAudio");
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+window.onload = showCoders();
